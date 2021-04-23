90d48850c3acfcd1038e31347e83cb3315a2523f
jpfcabral <jpfcabral@hotmail.com>
2021-04-23 19:41:12 -0300

    add Widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,11 +4,6 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
-
-    connect(ui->pushButtonQuit,
-            SIGNAL(clicked(bool)),
-            this,
-            SLOT(QuitFromApp()));
 }
 
 MainWindow::~MainWindow(){
